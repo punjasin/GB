d366fbc6817194b3ff6e409792c1142bffddcc0f
punjasin <punjasin_p@cmu.ac.th>
2015-09-06 00:30:04 +0700

    First Create
    nothing implement yet

---
+++ src/main/java/GB/GourmetBookerApplication.java
@@ -0,0 +1,12 @@
+package GB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GourmetBookerApplication.class, args);
+    }
+}
